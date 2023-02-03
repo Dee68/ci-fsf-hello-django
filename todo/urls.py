32d9da151378451d9168c60d0de6e8925911df0f
Dee68 <ddimie283@gmail.com>
2023-02-03 10:41:50 +0000

    Add crud functionality.

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,8 @@ app_name = 'todo'
 
 urlpatterns = [
     path('', views.get_todo_list, name='home'),
+    path('add', views.add_item, name='add'),
+    path('edit/<itemId>', views.edit_item, name='edit'),
+    path('toggle/<itemId>', views.toggle_item, name='toggle'),
+    path('delete/<itemId>', views.delete_item, name='delete')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,46 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
+
+
 def get_todo_list(request):
-    context = {}
+    items = Item.objects.all()
+    context = {'items': items}
     return render(request, 'todo/home.html', context)
+
+
+def add_item(request):
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo:home')
+    form = ItemForm()
+    context = {'form': form}
+    return render(request, 'todo/add_item.html', context)
+
+
+def edit_item(request, itemId):
+    item = get_object_or_404(Item, id=itemId)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('todo:home')
+    form = ItemForm(instance=item)
+    return render(request, 'todo/edit_item.html', context={'form': form})
+
+
+def toggle_item(request, itemId):
+    item = get_object_or_404(Item, id=itemId)
+    item.done = not item.done
+    item.save()
+    return redirect('todo:home')
+
+
+def delete_item(request, itemId):
+    item = get_object_or_404(Item, id=itemId)
+    item.delete()
+    return redirect('todo:home')
